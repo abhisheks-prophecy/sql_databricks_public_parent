79592864988386d92d8c32196b4470ed828a3ea0
abhisheks@prophecy.io <abhisheks@prophecy.io>
2026-07-30 07:01:37 +0000

    Update SQL_DB_Parent_AirflowComposerJob DBT invocation settings
    - Set sqlFabricId to 12042
    - Add run_props flag and DBT_PROFILES_DIR env for Model_0 DBT execution

--- jobs/SQL_DB_Parent_AirflowComposerJob/code/staging_abhisheks_e2etests_sql_parentdatabricks_sanity_sql_db_parent_airflowcomposerjob/tasks/Model_0.py
+++ jobs/SQL_DB_Parent_AirflowComposerJob/code/staging_abhisheks_e2etests_sql_parentdatabricks_sanity_sql_db_parent_airflowcomposerjob/tasks/Model_0.py
@@ -29,8 +29,8 @@ def Model_0():
           "select": "",
           "threads": "",
           "exclude": "",
-          "run_props": "",
-          "envs": {"DBT_DATABRICKS_INVOCATION_ENV" : "prophecy"},
+          "run_props": " --profile run_profile",
+          "envs": {"DBT_DATABRICKS_INVOCATION_ENV" : "prophecy", "DBT_PROFILES_DIR" : "/home/airflow/gcs/data"},
           "project_config": None
         },
         retries = 0
